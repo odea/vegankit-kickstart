3bd5de5e8ae8ce265e7958a879920334932131a3
Adam Kochanowicz <ajkochanowicz@gmail.com>
2015-02-10 18:00:54 -0500

    Adding some copy

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
     gulp.watch('./lib/sass/**/*.sass', ['sass', 'minify']);
     gulp.watch('./lib/images/**', ['images']);
-    gulp.watch('./lib/jade/**/*.jade', ['jade']);
+    gulp.watch('./lib/jade/**/*.jade', ['jade', 'mustache']);
     gulp.watch('./i18n/**/*.json', ['mustache']);
     gulp.watch(['./lib/coffee/**/*.coffee'], ['browserify', 'minify']);
     gulp.watch(['./lib/jade/tests/**', './lib/tests/**'], ['test']);
